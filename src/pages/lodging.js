3266a9748dc2dcaea1b3e5f98fc9e8890690ef20
WaltGurley <walt.gurley@gmail.com>
2019-06-27 15:55:48 -0400

    added lodging info

--- src/pages/lodging.js
+++ src/pages/lodging.js
@@ -6,6 +6,9 @@ export default () => (
   <Layout>
     <SEO title="Lodging" />
     <h1>Lodging</h1>
-    <p>Lodging information will be coming soon...</p>
+    <p>A block of rooms has been reserved for October 8 - October 11 at the <a href="https://hamptoninn3.hilton.com/en/hotels/louisiana/hampton-inn-new-orleans-st-charles-ave-garden-district-MSYGDHX/index.html">Hampton Inn New Orleans-St. Charles Ave./Garden District
+</a> at a rate of $159.00/night. The special room rate will be available until September 9 or until the group block is sold-out, whichever comes first.</p>
+    <p>To book a room at this special rate visit the <a href="https://hamptoninn.hilton.com/en/hp/groups/personalized/M/MSYGDHX-DLS-20191008/index.jhtml?WT.mc_id=POG">Hampton Inn SEDLS reservation site</a> and use the group code <strong>DLS</strong>.</p>
+    <p>Guests needing ADA compliant rooms should contact the hotel directly at <a href="tel:+1 (504) 899-9990">+1 (504) 899-9990</a>.</p>
   </Layout>
 )
