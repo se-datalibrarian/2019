4b310e67eaf13d31c35a5dd8529f324aef39ed58
WaltGurley <walt.gurley@gmail.com>
2019-07-25 10:18:50 -0400

    fixed typo

--- src/pages/lodging.js
+++ src/pages/lodging.js
@@ -13,7 +13,7 @@ export default () => (
     <p>The special room rate will be available until September 9 or until the group block is sold-out, whichever comes first. To book a room visit the <a href="https://hamptoninn.hilton.com/en/hp/groups/personalized/M/MSYGDHX-DLS-20191008/index.jhtml?WT.mc_id=POG">Hampton Inn SEDLS reservation site</a> and use the group code <strong>DLS</strong>. Guests needing ADA compliant rooms should contact the hotel directly at <a href="tel:+1 (504) 899-9990">(504) 899-9990</a>.</p>
 
     <h2>Pontchartrain Hotel</h2>
-    <p>A block of 10 rooms has been reserved for October 9 - October 10 at the <a href="http://thepontchartrainhotel.com/">Pontchartrain Hotel</a> at a rate of $189.00/night. The Pontchartrain Hotel is approximately 3 miles from the symposium site and is within a one minute walk of the St. Charles Streetcar stop at St. Charles at Josephine. See the map below for more information.</p>
+    <p>A block of 10 rooms has been reserved for October 9 - October 10 at the <a href="http://thepontchartrainhotel.com/">Pontchartrain Hotel</a> at a rate of $189.00/night. The Pontchartrain Hotel is approximately three miles from the symposium site and is within a one minute walk of the St. Charles Streetcar stop at St. Charles at Josephine. See the map below for more information.</p>
 
     <p>To book a room at this special rate visit the <a href="https://gc.synxis.com/rez.aspx?Chain=19060&arrive=10/9/2019&depart=10/11/2019&adult=2&child=0&group=2473002">Pontchartrain Hotel reservation site</a> and use the group code <strong>2473002</strong> or call <a href="tel:+1 (504) 323-1425">(504) 323-1425</a> between 8am and 5pm, Monday to Friday, and mention the Southeast Data Librarian Symposium.</p>
 
