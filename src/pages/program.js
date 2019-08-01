3a21057036b8b30d245ed44b0a2e9eb6811048a8
WaltGurley <walt.gurley@gmail.com>
2019-08-01 16:34:30 -0400

    removed call for proposals text/button after closing date

--- src/pages/program.js
+++ src/pages/program.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import buttons from '../components/buttons.module.css'
 import SEO from "../components/seo"
 
 export default () => (
   <Layout>
     <SEO title="Program" />
     <h1>Program</h1>
-    <p>The Southeast Data Librarian Symposium (SEDLS) is currently accepting proposals for workshops, short talks, and poster presentations for SEDLS 2019! We are also seeking prospective panelists on several topics.</p>
-    <a className={`${buttons.buttonLink} ${buttons.centerButton}`} href="https://docs.google.com/forms/d/e/1FAIpQLSeQbqCugN7tCuu1NCc6Fs3V2aYW0YfQFRkO1CLJtgFx05-WFw/viewform?usp=sf_link">Submit a proposal</a>
+    <p>The SEDLS 2019 call for proposals has ended. The program committee is in the process of reviewing submissions and will update this page with a program soon.</p>
   </Layout>
 )
